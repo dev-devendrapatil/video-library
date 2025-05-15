9447527d91f8459a67acecf222ff145440c0901f
Devendra Patil <dev.devendrapatil@gmail.com>
2025-05-15 21:05:22 +0530

    added thst utils and project setup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import { connectDB } from './db/index.js'
+import dotenv from "dotenv";
+import { connectDB } from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-  path:"./env/.env"
-})
-const app = express()
-const PORT = process.env.PORT || 3000
+  path: "./env/.env",
+});
+const PORT = process.env.PORT || 3000;
 connectDB()
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-}
-)
-
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log("server is running on port", PORT);
+    })
+  )
+  .catch((error) => {
+    console.log("problem in connect to db", error.message);
+  });
