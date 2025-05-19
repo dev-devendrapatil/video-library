0b05434ff14303c2797246f302fd009a2c781d0d
Devendra Patil <dev.devendrapatil@gmail.com>
2025-05-19 23:06:25 +0530

    Fix the bug and validation

--- src/validators/user/user.update.validator.js
+++ src/validators/user/user.update.validator.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const userUpdateValidator = Joi.object({
-  userName: Joi.string().trim().alphanum().min(3).max(30),
-  fullName: Joi.string().trim().min(2).max(50),
-  email: Joi.string().trim().email(),
-}).xor('userName','fullName','email');
+  userName: Joi.string().trim().alphanum().min(3).max(30).optional(),
+  fullName: Joi.string().trim().min(2).max(50).optional(),
+  email: Joi.string().trim().email().optional(),
+}).or('userName', 'fullName', 'email');
